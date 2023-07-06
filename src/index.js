3073cd2fc2dd67804d271ec59b38d036459de8e7
HongCam <93567201+HongCam23@users.noreply.github.com>
2023-07-06 12:10:29 +0700

    Update root
    Update root
    
    Co-Authored-By: DilysNgo <ngohung2395@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import configureStore from './state/store';
 import reportWebVitals from './reportWebVitals';
 
 const initialState = {};
+
 const store = configureStore(initialState);
 
 ReactDOM.render(
